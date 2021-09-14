5815d0fc2f11bf1f14d4db2b0b1b32dd5450d9d8
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-09-14 01:41:35 -0700

    Setting up the basics for ui.

--- script.py
+++ script.py
@@ -9,3 +9,33 @@ Search for an entry
 Add, Update, Delete & Close
 """
 
+from tkinter import *
+
+window = Tk()
+
+l1 = Label(window, text="Title")
+l1.grid(row=0, column=0)
+
+l2 = Label(window, text="Author")
+l2.grid(row=0, column=2)
+
+l3 = Label(window, text="Year")
+l3.grid(row=1, column=0)
+
+l4 = Label(window, text="ISBN")
+l4.grid(row=1, column=2)
+
+title_text = StringVar()
+e1 = Entry(window, textvariable=title_text)
+e1.grid(row=0, column=1)
+
+author_text = StringVar()
+e2 = Entry(window, author_text)
+e2.grid()
+
+year_text = StringVar()
+e3 = Entry(window, year_text)
+e3.grid()
+
+
+window.mainloop()
